e85fad2396e8e851778433c532dbb87ea61688b2
Janderson Angelo <jandersonangelo@hotmail.com>
2019-12-19 17:00:57 -0300

    adding discord

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const cheerio = require("cheerio");
 const cloudscraper = require("cloudscraper");
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
-cloudscraper
-  .get("https://www.tibia.com/community/?subtopic=worlds&world=Celebra")
-  .then(r => {
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+const fetchPlayers = async world => {
+  const characters = [];
+  try {
+    const r = await cloudscraper.get(
+      "https://www.tibia.com/community/?subtopic=worlds&world=Celebra"
+    );
     const $ = cheerio.load(r);
-    // console.log($('tr .Odd')[0].children[0].children[1].children[0].data)
     $("tbody")["5"].children.forEach((tr, index) => {
       if (index === 0) return;
       if (tr.type !== "tag") return;
@@ -14,17 +22,40 @@ cloudscraper
       const linkTag = tr.children[0].children[0];
       const nameTag =
         linkTag.attribs.name === undefined ? linkTag : linkTag.next;
-      const charName = nameTag.children[0].data;
-      const charUrl = nameTag.attribs.href;
-      const charLevel = parseInt(tr.children[1].children[0].data, 10);
-      const charVocation = tr.children[2].children[0].data;
+      const name = nameTag.children[0].data;
+      const url = nameTag.attribs.href;
+      const level = parseInt(tr.children[1].children[0].data, 10);
+      const vocation = tr.children[2].children[0].data;
       const character = {
-        charName,
-        charUrl,
-        charLevel,
-        charVocation
+        name,
+        url,
+        level,
+        vocation
       };
-      console.log(character);
+      characters.push(character);
     });
-  })
-  .catch(e => console.log(e));
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+  return characters;
+};
+
+client.on("message", async message => {
+  if (message.content === "!online") {
+    const characters = await fetchPlayers();
+    const resp = characters.map(char => {
+      return [
+        `Nome: ${char.name}`.padEnd(35, " "),
+        `Level: ${char.level}`.padEnd(4, "  "),
+        `Vocação: ${char.vocation}`
+      ].join("  |  ");
+    });
+    console.log(resp.length);
+    message.channel.send(resp, { split: true });
+  }
+});
+
+client
+  .login("NjU2OTU1NDA5NjU5MDY4NDY0.XfqLjg.7ybjoNRRwry0sOdW3qAAHF2g1Hk")
+  .catch(e => console.error(e));
